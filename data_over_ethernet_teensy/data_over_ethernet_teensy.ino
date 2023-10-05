78b9001b074c26c522c9f30249eed71930711e04
Raj <rajdrana03@gmail.com>
2023-10-05 14:17:44 +0530

    added teensy ethernet

--- data_over_ethernet_teensy/data_over_ethernet_teensy.ino
+++ data_over_ethernet_teensy/data_over_ethernet_teensy.ino
@@ -12,16 +12,16 @@ IPAddress subnet(255, 255, 255, 0);  // Subnet mask (same as your laptop)
 IPAddress dns(8, 8, 8, 8);           // DNS server (e.g., Google DNS)
 
 
-IPAddress serverIp(192, 168, 0, 140);  // IP address of your PV server
-int serverPort = 53;                // Port of your PV server
+IPAddress serverIp(192, 168, 0, 129);  // IP address of your PV server
+int serverPort = 53;                   // Port of your PV server
 
-uint8_t count[100];
+uint16_t count = 1234;
 long timer1, timer2;
 
 EthernetClient client;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(1500000);
   delay(1000);
   Serial.println("Setup started");
   // Start Ethernet connection
@@ -31,10 +31,10 @@ void setup() {
   Serial.println(i);
   // Wait for Ethernet to initialize
   delay(1000);
-  for(int i=0; i<100; i++)
-  {
-    count[i]=125;
-  }
+  // for(int i=0; i<100; i++)
+  // {
+  //   count[i]=125;
+  // }
   Serial.println("Setup done");
 }
 
@@ -52,19 +52,21 @@ void loop() {
     if (!client.connected()) {
       // Connect to the PV server
       if (client.connect(serverIp, serverPort)) {
+
         Serial.println("Connected to PC server");
         // client.println("Hello, PC server!");  // Send your data here
         // delay(2000);
-        // timer1 = micros();
-        // for (int i = 0; i < 1000; i++) {
-          client.write((uint8_t*)&count, 100);
-        // }
-        // timer2 = micros();
+        timer1 = micros();
+        for (int i = 0; i < 1000; i++) {
+          client.write((uint8_t*)&count, 2);
+        }
+        timer2 = micros();
         // delay(3000);
-        // long diff = timer2 - timer1;
-        // Serial.println(diff);
-        // while (1)
-        //   ;
+        long diff = timer2 - timer1;
+        Serial.println(diff);
+        while (1)
+          ;
+
       } else {
         Serial.println("Connection failed");
       }
@@ -89,28 +91,3 @@ void loop() {
 }
 
 
-
-// #include <Ethernet.h>
-
-// byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-// IPAddress ip(192, 168, 1, 100);
-// EthernetServer server(80);
-
-// void setup() {
-//   Ethernet.begin(mac, ip);
-//   server.begin();
-// }
-
-// void loop() {
-//   EthernetClient client = server.available();
-
-//   if (client) {
-//     if (client.connected()) {
-//       client.println("HTTP/1.1 200 OK");
-//       client.println("Content-Type: text/html");
-//       client.println();
-//       client.println("<html><body><h1>Hello, World!</h1></body></html>");
-//       client.stop();
-//     }
-//   }
-// }
--- mpu9250_6channel_only_raw_data/mpu9250_6channel_only_raw_data.ino
+++ mpu9250_6channel_only_raw_data/mpu9250_6channel_only_raw_data.ino
@@ -93,78 +93,116 @@
 
 
 // #include "esp_task_wdt.h"
-#include <MPU9250_WE.h>
+// #include <MPU9250_WE.h>
+// #include <SPI.h>
+
+// /////////////MPU Setup/////////////
+// //VSPI
+// #define MPU_CS_PIN 5
+
+// const int8_t SYNC_BYTE = 0xAA;
+// int16_t Ax{ 0 };
+// int16_t Ay{ 0 };
+// int16_t Az{ 0 };
+
+// int16_t Gx{ 0 };
+// int16_t Gy{ 0 };
+// int16_t Gz{ 0 };
+
+// //long timer1, timer2;
+
+// ///////////////CONSTRUCTORS//////////////
+// MPU9250_WE myMPU9250 = MPU9250_WE(&SPI, MPU_CS_PIN, true);  //VSPI
+
+// void setup() {
+//   Serial.begin(1500000);
+//   //  Serial.begin(115200);
+//   esp_task_wdt_delete(NULL);
+
+//   // vTaskSuspend(NULL);
+
+//   myMPU9250.init();
+//   delay(1000);
+//   myMPU9250.autoOffsets();
+//   myMPU9250.disableGyrDLPF(MPU9250_BW_WO_DLPF_8800);  // bandwdith without DLPF
+//   // myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_500);
+//   myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
+//   myMPU9250.setAccRange(MPU9250_ACC_RANGE_4G);
+//   myMPU9250.enableAccDLPF(false);
+//   myMPU9250.setAccDLPF(MPU9250_DLPF_6);
+//   myMPU9250.setMagOpMode(AK8963_CONT_MODE_100HZ);
+//   delay(1000);
+// }
+
+// void loop() {
+//   xyzFloat gValue = myMPU9250.getGValues();
+//   xyzFloat gyr = myMPU9250.getGyrValues();
+//   for (;;) {
+//     esp_task_wdt_init(10, false);
+//     //    timer1 = micros();
+
+//     gValue = myMPU9250.getGValues();
+//     gyr = myMPU9250.getGyrValues();
+//     Ax = gValue.x * 1000;
+//     Ay = gValue.y * 1000;
+//     Az = gValue.z * 1000;
+
+//     Gx = gyr.x * 10;
+//     Gy = gyr.y * 10;
+//     Gz = gyr.z * 10;
+
+//     //  timer2 = micros();
+//     //  Serial.println(timer2 - timer1);
+
+// Serial.write(SYNC_BYTE);  // Send the start/sync byte
+
+// Serial.write((uint8_t*)&(Ax), sizeof(Ax));
+// Serial.write((uint8_t*)&(Ay), sizeof(Ay));
+// Serial.write((uint8_t*)&(Az), sizeof(Az));
+
+// Serial.write((uint8_t*)&(Gx), sizeof(Gx));
+// Serial.write((uint8_t*)&(Gy), sizeof(Gy));
+// Serial.write((uint8_t*)&(Gz), sizeof(Gz));
+
+//   }
+// }
+
+
 #include <SPI.h>
 
-/////////////MPU Setup/////////////
-//VSPI
-#define MPU_CS_PIN 10
+#define CS_PIN 5
+#define ACCEL_XOUT_H 0x3B
+#define GYRO_XOUT_H 0x43
+#define PWR_MGMT_1 0x6B
 
 const int8_t SYNC_BYTE = 0xAA;
-int16_t Ax{ 0 };
-int16_t Ay{ 0 };
-int16_t Az{ 0 };
-
-int16_t Gx{ 0 };
-int16_t Gy{ 0 };
-int16_t Gz{ 0 };
-
-//long timer1, timer2;
-
-///////////////CONSTRUCTORS//////////////
-MPU9250_WE myMPU9250 = MPU9250_WE(&SPI, MPU_CS_PIN, true);  //VSPI
+int16_t a[6];  //, g[3];
 
 void setup() {
-  Serial.begin(1500000);
-  //  Serial.begin(115200);
-  // esp_task_wdt_delete(NULL);
-
-  // vTaskSuspend(NULL);
-
-  myMPU9250.init();
-  delay(1000);
-  myMPU9250.autoOffsets();
-  myMPU9250.disableGyrDLPF(MPU9250_BW_WO_DLPF_8800);  // bandwdith without DLPF
-  // myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_500);
-  myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
-  myMPU9250.setAccRange(MPU9250_ACC_RANGE_4G);
-  myMPU9250.enableAccDLPF(false);
-  myMPU9250.setAccDLPF(MPU9250_DLPF_6);
-  myMPU9250.setMagOpMode(AK8963_CONT_MODE_100HZ);
-  delay(1000);
+  Serial.begin(115200);
+  SPI.begin();
+  pinMode(CS_PIN, OUTPUT);
+  digitalWrite(CS_PIN, HIGH);
+  SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE0));
+  SPI.transfer(PWR_MGMT_1);
+  SPI.transfer(0);
 }
 
 void loop() {
-  xyzFloat gValue = myMPU9250.getGValues();
-  xyzFloat gyr = myMPU9250.getGyrValues();
-  for (;;) {
-    // esp_task_wdt_init(10, false);
-    //    timer1 = micros();
-
-    gValue = myMPU9250.getGValues();
-    gyr = myMPU9250.getGyrValues();
-    Ax = gValue.x * 1000;
-    Ay = gValue.y * 1000;
-    Az = gValue.z * 1000;
-
-    Gx = gyr.x * 100;
-    Gy = gyr.y * 100;
-    Gz = gyr.z * 100;
-
-    //  timer2 = micros();
-    //  Serial.println(timer2 - timer1);
-
-    Serial.write(SYNC_BYTE);  // Send the start/sync byte
-
-    Serial.write((uint8_t*)&(Ax), sizeof(Ax));
-    Serial.write((uint8_t*)&(Ay), sizeof(Ay));
-    Serial.write((uint8_t*)&(Az), sizeof(Az));
-
-    Serial.write((uint8_t*)&(Gx), sizeof(Gx));
-    Serial.write((uint8_t*)&(Gy), sizeof(Gy));
-    Serial.write((uint8_t*)&(Gz), sizeof(Gz));
-
-    // delay(1);
-    // delayMicroseconds(1);
+  SPI.transfer(ACCEL_XOUT_H | 0x80);
+  for (int i = 0; i < 6; i++) {
+    a[i] = SPI.transfer(0) << 8 | SPI.transfer(0);
   }
+
+  // Serial.write(SYNC_BYTE);  // Send the start/sync byte
+
+  // Serial.write((uint8_t*)&(a[0]), sizeof(a[0]));
+  // Serial.write((uint8_t*)&(a[1]), sizeof(a[1]));
+  // Serial.write((uint8_t*)&(a[2]), sizeof(a[2]));
+
+  // Serial.write((uint8_t*)&(a[3]), sizeof(a[3]));
+  // Serial.write((uint8_t*)&(a[4]), sizeof(a[4]));
+  // Serial.write((uint8_t*)&(a[5]), sizeof(a[5]));
+  Serial.printf("Accel: %d %d %d, Gyro: %d %d %d\n", a[0], a[1], a[2], a[3], a[4], a[5]);// g[0], g[1], g[2]);
+  delay(1000);
 }
